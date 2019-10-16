099e843bf0678a868c8115c7ac7636b3cf9e38cb
Ben <bigriffin94@outlook.com>
2019-10-16 09:40:37 -0700

    Token Working Token is returned upon login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // Create JWT token
+        const token = generateToken(user);
+        // add token to response
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,22 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+// Function that gives us 
+function generateToken(user) {
+  const payload = {
+    username: user.username, // 
+    subject: user.id, // sub-claim on the token, same concept as HASHing
+  };
+
+  const secret = 'is it secret?';
+
+  const options = {
+    expiresIn: '1h', // adds a claim of the expiration date
+  }
+
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
